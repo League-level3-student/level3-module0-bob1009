72db7dc76887b152ad61269f6dbb808acd50024e
ilabstaff <ilabstaff@172.16.116.128>
2019-10-01 17:54:59 -0700

    finished firework project and array methods project, working on more array fun

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -1,43 +1,53 @@
 package _01_Simple_Array_Algorithms;
 
 public class _00_1D_Array_Methods {
-	//1. Complete the method so that it returns the sum of all
-	//   of the integers in the array being passed in
+	// 1. Complete the method so that it returns the sum of all
+	// of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-	int add=0;
+		int add = 0;
 		for (int i = 0; i < values.length; i++) {
-		add+=values[i];
+			add += values[i];
+		}
+		return add;
 	}
-		return add;   
-	}
-	
-	//2. Complete the method so that it returns the average of all
-	//   of the integers in the array being passed in
-	
+
+	// 2. Complete the method so that it returns the average of all
+	// of the integers in the array being passed in
+
 	public static double averageIntArray(int[] values) {
-	double store=0;
-	for (int i = 0; i < values.length; i++) {
-		store += values[i];
-		
+		double store = 0;
+		for (int i = 0; i < values.length; i++) {
+			store += values[i];
+
+		}
+		store /= values.length;
+		return store;
 	}
-		store/=values.length;
-	return store;
-	}
-	
-	
-	//3. Complete the method so that it returns true if the integer
-	//   array contains the value specified by the second parameter.
-	//   It should otherwise return false.
+
+	// 3. Complete the method so that it returns true if the integer
+	// array contains the value specified by the second parameter.
+	// It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
-		return false;
-	}
-	
-	//4. Complete the method so that it returns the index of the,
-	//   first instance that the specified value occurs in the array.
-	//   If the array does not contain the specified value, it should return -1.
-	public static int getIndex(int[] arr, int value) {
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == value) {
+				return true;
+			} 
+			}
+		 
+				return false;
 		
-		return 0;
+
+	}
+
+	// 4. Complete the method so that it returns the index of the
+	// first instance that the specified value occurs in the array.
+	// If the array does not contain the specified value, it should return -1.
+	public static int getIndex(int[] arr, int value) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i]==value) {
+				return i;
+			}
+		}
+		return -1;
 	}
 }
