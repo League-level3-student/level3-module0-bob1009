2cdd25561d92dc783f6aa359ddee99113008385f
ilabstaff <ilabstaff@172.16.116.128>
2019-10-01 17:55:42 -0700

    working on more array fun

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -5,15 +5,28 @@ import java.util.Random;
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
 	
+	public static void main(String[] args) {
+		String[]meow= {"1","2","3","4"};
+		writearray (meow);
+	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
 	
+	static void writearray(String[]array) {
+		 for (int i = 0; i < array.length; i++) {
+			System.out.println(array[i]);
+		}
+	}
 	
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
+	void meme( String[]array) {
+		for (int i = 0; i > array.length; i--) {
+			System.out.println(array[i]);
+		}
+	}
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
